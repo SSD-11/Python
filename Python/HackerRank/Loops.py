2f8ef47be9c418028581d650501feb33afc4fed9
[SSD] <[samuserna2005@gmail.com]>
2022-09-07 12:25:29 -0500

    Ejercicios realizados de HackerRank

---
+++ Python/HackerRank/Loops.py
@@ -0,0 +1,6 @@
+#The provided code stub reads and integer, , from STDIN. For all non-negative integers
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
