ea0d69989c4e6dbc81857f51f6034b25288f863d
Mistic <66537888+SSD-11@users.noreply.github.com>
2022-09-08 23:45:57 -0500

    Delete Python/HackerRank directory

--- Python/HackerRank/PrintFunction.py
+++
@@ -1,7 +0,0 @@
-#The included code stub will read an integer, , from STDIN.
-#Without using any string methods, try to print the following:
-
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(1, n+1):
-        print(i, end="")
